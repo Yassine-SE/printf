87c41d4b1d1b01100ef0b73b93bffb2a27c01105
Yassine-SE <ytarkaoui@gmail.com>
2023-10-14 10:22:40 +0100

    Added _printf

---
+++ _printf.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "stdio.h"
+#include "stdarg.h"
+
+/**
+ * _printf - function
+ * @format: param
+ * @...: param
+ *
+ * Return: 0 success
+*/
+int _printf(const char *format, ...)
+{
+	
+}
