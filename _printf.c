b52f35a58f05fc6169ce3d6251b7693cb1be050e
Tonnamiacart <tonnamiacart@gmail.com>
2023-10-14 21:18:08 +0100

    Task 0

--- _printf.c
+++ _printf.c
@@ -1,68 +1,59 @@
 #include "main.h"
 
 /**
- * _printf - Custom printf function
- * @format: Format string
- * @...: Additional arguments
+ * _printf - function
+ * @format: param
+ * @...: param
  *
- * Return: Number of characters printed (excluding null byte)
- */
+ * Return: 0 success
+*/
 int _printf(const char *format, ...)
 {
-    char *string;
-    char character;
-    int i, j;
-    int count_out = 0;
-    va_list args_l;
+	char *string;
+	char character;
+	int i, j;
+	int count_out = 0;
+	va_list args_l;
 
-    if (!format)
-        return (-1);
-
-    va_start(args_l, format);
-
-    for (i = 0; format && format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++; // Move past '%'
-
-            if (format[i] == 'c')
-            {
-                character = va_arg(args_l, int);
-                _putchar(character);
-                count_out++;
-            }
-            else if (format[i] == 's')
-            {
-                string = va_arg(args_l, char *);
-                if (string == NULL)
-                    string = "(null)";
-                while (*string)
-                {
-                    _putchar(*string);
-                    count_out++;
-                    string++;
-                }
-            }
-            else if (format[i] == '%')
-            {
-                _putchar('%');
-                count_out++;
-            }
-            else
-            {
-                _putchar('%');
-                _putchar(format[i]);
-                count_out += 2;
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-            count_out++;
-        }
-    }
-
-    va_end(args_l);
-    return (count_out);
+	if (!format)
+		return (-1);
+	
+	va_start(args_l, format);
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] == '%' && format[i + 1] == 'c')
+		{
+			character = va_arg(args_l, int);
+			_putchar(character);
+			count_out++;
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == 's')
+		{
+			string = va_arg(args_l, char*);
+			count_out += _strlen(string);
+			for (j = 0; string[j] != '\0'; j++)
+				_putchar(string[j]);
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			_putchar(format[i]);
+			count_out++;
+			i++;
+		}
+		else if (format[i] == '%' && (format[i + 1] != 'c' || format[i + 1] == 's' || format[i + 1] != '%'))
+		{
+			_putchar(format[i + 1]);
+			count_out++;
+			i++;
+		}
+		else
+		{
+			_putchar(format[i]);
+			count_out++;
+		}
+	}
+	va_end(args_l);
+	return (count_out);
 }
