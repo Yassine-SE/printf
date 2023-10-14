df6d24d654ce22c8e31e13c32351c94a6ea13792
Yassine-SE <ytarkaoui@gmail.com>
2023-10-14 13:08:22 +0100

    _prinf v1.2

--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "stdio.h"
 #include "stdarg.h"
+#include <unistd.h>
 
 /**
  * _printf - function
@@ -11,22 +12,53 @@
 */
 int _printf(const char *format, ...)
 {
-	int i;
-	unsigned int count_sp = 0;
+	char *string;
+	char character;
+	int i, j;
+	char specifiers[] = {'c', 's'};
+	unsigned int count_sp = 0, count_out = 0;
+	va_list args_l;
 
-	/* Loop through the "format" */
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		/* Check for specifiers "%" */
-		if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's'))
+		if (format[i] == '%')
 		{
-			/* Count specifiers */
-			count_sp++;
+			for (j = 0; i < 2; j++)
+			{
+				if (format[i + 1] == specifiers[j])
+					count_sp++;
+			}
 		}
 	}
 
+	va_start(args_l, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%' && format[i + 1] == 'c')
+		{
+			character = va_arg(args_l, int);
+			write(1, &character, 1);
+			count_out++;
+			i = i + 2;
+		}
+		if (format[i] == '%' && format[i + 1] == 's')
+		{
+			string = va_arg(args_l, char*);
+			count_out += _strlen(string);
+			for (j = 0; string[j] != '\0'; j++)
+			{
+				write(1, &string[j], 1);
+			}
+			i = i + 2;
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count_out++;
+		}
+	}
 
-
-
-
+	va_end(args_l);
+	write(1, '\0', 1);
+	return (count_out);
 }
---
+++ main.c
@@ -0,0 +1,16 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char name = "Yassine";
+
+    _printf("My name is %s.\n", name);
+    return (0);
+}
