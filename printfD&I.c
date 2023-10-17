d980eb3622c7d12fefbba15c81073752bf7c4b2a
Tonnamiacart <tonnamiacart@gmail.com>
2023-10-17 09:41:53 +0100

    task 1

--- printfD&I.c
+++ printfD&I.c
@@ -11,4 +11,9 @@ int _printf(const char *format, ...)
   int count_out = 0;
 	char *fmt_str;
 	va_list args_l;
+
+  if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	va_start(args_l, format);
+	fmt_str = (char *)format;
 }
